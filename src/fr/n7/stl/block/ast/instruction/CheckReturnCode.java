ce031e1e250cb23299bb0ed9922f05556a5fb2f8
Hathoute <whitesmith.thedj@gmail.com>
2022-04-11 01:07:51 +0200

    Initial commit

---
+++ src/fr/n7/stl/block/ast/instruction/CheckReturnCode.java
@@ -0,0 +1,7 @@
+package fr.n7.stl.block.ast.instruction;
+
+public enum CheckReturnCode {
+    TYPE_MISMATCH,
+    CONTINUE,
+    FINISHED
+}
---
+++ src/fr/n7/stl/block/ast/instruction/Conditional.java
@@ -0,0 +1,116 @@
+/**
+ * 
+ */
+package fr.n7.stl.block.ast.instruction;
+
+import java.util.Optional;
+
+import fr.n7.stl.block.ast.Block;
+import fr.n7.stl.block.ast.SemanticsUndefinedException;
+import fr.n7.stl.block.ast.expression.Expression;
+import fr.n7.stl.block.ast.scope.Declaration;
+import fr.n7.stl.block.ast.scope.HierarchicalScope;
+import fr.n7.stl.block.ast.type.AtomicType;
+import fr.n7.stl.block.ast.type.Type;
+import fr.n7.stl.tam.ast.Fragment;
+import fr.n7.stl.tam.ast.Register;
+import fr.n7.stl.tam.ast.TAMFactory;
+
+/**
+ * Implementation of the Abstract Syntax Tree node for a conditional instruction.
+ * @author Marc Pantel
+ *
+ */
+public class Conditional implements Instruction {
+
+	protected Expression condition;
+	protected Block thenBranch;
+	protected Block elseBranch;
+
+	public Conditional(Expression _condition, Block _then, Block _else) {
+		this.condition = _condition;
+		this.thenBranch = _then;
+		this.elseBranch = _else;
+	}
+
+	public Conditional(Expression _condition, Block _then) {
+		this.condition = _condition;
+		this.thenBranch = _then;
+		this.elseBranch = null;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "if (" + this.condition + " )" + this.thenBranch + ((this.elseBranch != null)?(" else " + this.elseBranch):"");
+	}
+	
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.instruction.Instruction#collect(fr.n7.stl.block.ast.scope.Scope)
+	 */
+	@Override
+	public boolean collectAndBackwardResolve(HierarchicalScope<Declaration> _scope) {
+		if(elseBranch != null){
+			return condition.collectAndBackwardResolve(_scope) && thenBranch.collect(_scope) && elseBranch.collect(_scope);
+		}
+		return condition.collectAndBackwardResolve(_scope) && thenBranch.collect(_scope);
+
+	}
+	
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.instruction.Instruction#resolve(fr.n7.stl.block.ast.scope.Scope)
+	 */
+	@Override
+	public boolean fullResolve(HierarchicalScope<Declaration> _scope) {
+		if(elseBranch != null) {
+			return condition.fullResolve(_scope) && thenBranch.resolve(_scope) && elseBranch.resolve(_scope);
+		}
+		return condition.fullResolve(_scope) && thenBranch.resolve(_scope);
+	}
+
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.Instruction#checkType()
+	 */
+	@Override
+	public boolean checkType() {
+		return this.condition.getType().equalsTo(AtomicType.BooleanType);
+	}
+
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.Instruction#allocateMemory(fr.n7.stl.tam.ast.Register, int)
+	 */
+	@Override
+	public int allocateMemory(Register _register, int _offset) {
+		if(elseBranch != null) {
+			return thenBranch.allocateMemory(_register, _offset) + elseBranch.allocateMemory(_register, _offset);
+		}
+		return thenBranch.allocateMemory(_register, _offset);
+	}
+
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.Instruction#getCode(fr.n7.stl.tam.ast.TAMFactory)
+	 */
+	@Override
+	public Fragment getCode(TAMFactory _factory) {
+		throw new SemanticsUndefinedException( "Semantics getCode is undefined in Conditional.");
+	}
+
+	@Override
+	public CheckReturnCode checkReturnType(Type type) {
+		CheckReturnCode thenCode = thenBranch.checkReturnType(type);
+		CheckReturnCode elseCode = elseBranch == null ? CheckReturnCode.CONTINUE : elseBranch.checkReturnType(type);
+
+		if(thenCode == CheckReturnCode.TYPE_MISMATCH || elseCode == CheckReturnCode.TYPE_MISMATCH) {
+			return CheckReturnCode.TYPE_MISMATCH;
+		}
+		else if(thenCode == CheckReturnCode.CONTINUE || elseCode == CheckReturnCode.CONTINUE) {
+			return CheckReturnCode.CONTINUE;
+		}
+		else {
+			return CheckReturnCode.FINISHED;
+		}
+	}
+
+}
